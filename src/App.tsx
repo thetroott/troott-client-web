4c76760dd6f940213aedb43bde9ef9fa22792a7c
Damola Oladipo <thedamolaoladipo@gmail.com>
2025-05-13 13:56:53 +0100

    vite migration step 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from "react";
+import { Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ErrorBoundary } from "react-error-boundary";
 

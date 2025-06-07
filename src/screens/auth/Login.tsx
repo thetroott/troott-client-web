3cce19b385b84365e1e98355a43b2dcbe970b431
Damola Oladipo <thedamolaoladipo@gmail.com>
2025-06-07 08:22:19 +0100

    feat: added register form and register screen

--- src/screens/auth/Login.tsx
+++ src/screens/auth/Login.tsx
@@ -8,11 +8,11 @@ const Login = () => {
       <div className="grid min-h-svh lg:grid-cols-2">
         <div className="flex flex-col gap-4 p-6 md:p-10">
           <div className="flex justify-center gap-2 md:justify-start">
-            <a href="#" className="flex items-center gap-2 font-medium">
+            <a href="/" className="flex items-center gap-2 font-medium">
               <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
                 <GalleryVerticalEnd className="size-4" />
               </div>
-              Acme Inc.
+              troott
             </a>
           </div>
           <div className="flex flex-1 items-center justify-center">

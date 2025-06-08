d3508ca441c90f615d8d86dbcc34d1cc4d35013a
Damola Oladipo <thedamolaoladipo@gmail.com>
2025-06-08 05:56:38 +0100

    feat: added verify with OTP page

--- src/screens/auth/Verification.tsx
+++ src/screens/auth/Verification.tsx
@@ -1,39 +1,17 @@
-import OtpForm from "@/components/containers/otp-form"
-import { GalleryVerticalEnd } from "lucide-react"
-
+import { AuthLayout } from "@/components/layouts/Authlayout";
+import OtpForm from "@/components/containers/otp-form";
 
 const Verification = () => {
   return (
-  
-     <>
-      <div className="grid min-h-svh lg:grid-cols-2">
-        <div className="flex flex-col gap-4 p-6 md:p-10">
-          <div className="flex justify-center gap-2 md:justify-start">
-            <a href="/" className="flex items-center gap-2 font-medium">
-              <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
-                <GalleryVerticalEnd className="size-4" />
-              </div>
-              troott
-            </a>
-          </div>
-          <div className="flex flex-1 items-center justify-center">
-            <div className="w-full max-w-xs">
-              <OtpForm />
-            </div>
-          </div>
-        </div>
-        <div className="relative hidden bg-muted lg:block">
-          <img
-            src="/images/assets/login-side.jpg"
-            alt="Image"
-            className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
-          />
-        </div>
-      </div>
-    
-      
+    <>
+      <AuthLayout
+        title="Enter verification code"
+        description="We sent a 6-digit code to your email address"
+      >
+        <OtpForm />
+      </AuthLayout>
     </>
-  )
-}
+  );
+};
 
-export default Verification
+export default Verification;

f7e6687c35683b83ed61c27ec19cba1998bd6a33
Temitope Okuselu <topeokuselu@gmail.com>
2025-08-13 21:45:29 +0100

    Update:Optimize Upload Options layout and postitioning

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
-import { Toaster } from "sonner"
+import { Toaster } from "sonner";
 
-<Toaster richColors position="top-center" />
+<Toaster richColors position="top-center" />;
 
 const App = () => {
   return (

6c6472a669323bb535d95f389bbd9e1cd9eddc76
damola <thedamolaoladipo@gmail.com>
2025-09-25 16:50:16 +0100

    chore: production fix (my-sermons)

--- src/screens/dashboard/MySermons.tsx
+++ src/screens/dashboard/MySermons.tsx
@@ -1,8 +1,6 @@
 import { dummySermons } from "@/_data/dummySermons";
 import EmptySermonsState from "@/components/containers/my-sermons/EmptySermonsState";
 import SermonsTable from "@/components/containers/my-sermons/SermonsTable";
-//import EmptySermonsState from "@/components/containers/dashboard/EmptySermonsState";
-//import SermonsTable from "@/components/containers/dashboard/SermonsTable";
 
 const Sermons = () => {
   const hasSermons = dummySermons.length > 0;
